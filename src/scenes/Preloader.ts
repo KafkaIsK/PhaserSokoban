594e58b94d0fdcd7707841f561031bd36050b29c
KafkaIsK <richlam4@gmail.com>
2021-06-18 19:43:32 +0100

    Initial commit

---
+++ src/scenes/Preloader.ts
@@ -0,0 +1,36 @@
+import Phaser from 'phaser'
+
+import WebFontFile from '../files/WebFontFile'
+
+export default class Preloader extends Phaser.Scene
+{
+	constructor()
+	{
+		super('preloader')
+	}
+
+	preload()
+	{
+		const fonts = new WebFontFile(this.load, [
+			'Poppins',
+			'Righteous'
+		])
+		this.load.addFile(fonts)
+
+		this.load.audio('game-music', 'assets/music/CaveLoop.wav');
+		this.load.audio('click', 'assets/sfx/click_002.ogg')
+		this.load.audio('confirmation', 'assets/sfx/confirmation_001.ogg')
+		this.load.audio('error', 'assets/sfx/error_006.ogg')
+		this.load.audio('move', 'assets/sfx/maximize_008.ogg')
+	}
+
+	create()
+	{
+		this.scene.start('game', { level: 4 });
+
+		this.sound.play('game-music', {
+			loop: true,
+			volume: 0.25
+		});
+	}
+}
